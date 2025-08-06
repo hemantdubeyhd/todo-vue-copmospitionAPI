89b17447e5d80527fce924629631c8fbdad0ebff
hemantdubeyhd <hemantdubeyhd@gmail.com>
2025-08-06 14:15:17 +0930

    adding and removinf items sucessfully

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,40 @@
 <template>
   <h1> Vue Compositon API Todo</h1>
 
-  <input v-mode="newItem" placeholder="Enter the new item.."/>
+  <input v-model="newItem" ref="itemInput" placeholder="Enter the new item.."/>
   <button @click="addItem">Add</button>
 
   <ul>
-    <li v-for="(item, index)" in items :key="index">
+    <li v-for="(item, index) in items" :key="index">
       {{ item }}
       <button @click="removeItem(index)">X</button>
     </li>
   </ul>
   </template>
-  
+
+
+
+  <script setup>
+  import {ref, nextTick} from 'vue';
+
+  const itemInput = ref(null);
+  const newItem = ref('');
+  const items = ref([]);
+
+  const addItem = () => 
+  {
+      if(newItem.value.trim() != '')
+    {
+      items.value.push(newItem.value.trim());
+      newItem.value = '';
+      
+      
+    }
+
+  };
+
+  const removeItem = (index) => 
+  {
+    items.value.splice(index, 1);
+  };
+</script>
