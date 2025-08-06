a6ca716943abb14bbf7613ce3f9f51e53404943b
hemantdubeyhd <hemantdubeyhd@gmail.com>
2025-08-06 15:29:55 +0930

    addedparent -chile components
    props and emit.
    added the component/child components in src\components

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,22 @@
+<template>
+    <li>
+        {{ item }}
+        <button @click="deleteItem">X</button>
+    </li>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+const props = defineProps({
+    item: String,
+    index: Number
+});
+
+const emit = defineEmits(['delete-Item'])
+
+const deleteItem = () =>
+{
+    emit('delete-item', props.index);
+}
+
+</script>
